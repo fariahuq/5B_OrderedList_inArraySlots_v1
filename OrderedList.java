8bcccc3c9148f0fbbe59c2b09fc92dfa8950d1a0
Faria Huq <46835579+fariahuq@users.noreply.github.com>
2019-04-03 11:13:17 -0400

    Add files via upload

---
+++ OrderedList.java
@@ -0,0 +1,17 @@
+public interface OrderedList {
+
+	
+	public String toString();
+
+
+	public void add( int value);
+
+
+	public int size();
+
+
+	public int get(int index);
+
+
+	public int remove(int index);
+}
